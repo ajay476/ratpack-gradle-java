aeee77890567bec8bdd9456d1caaef8aa34b910f
Ajay Gaddam <ajayreddy.smec@gmail.com>
2017-11-05 22:01:21 -0600

    Smartapp that integrates with ratpack

---
+++ smartapps/ajay476/apismartappdimmer.src/apismartappdimmer.groovy
@@ -0,0 +1,88 @@
+/**
+ *  APISmartAppDimmer
+ *
+ *  Copyright 2017 Ajay Gaddam
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "APISmartAppDimmer",
+    namespace: "ajay476",
+    author: "Ajay Gaddam",
+    description: "By endpoint URL path setting the Dimmer value",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+  section ("Allow external service to control these things...") {
+    input "DimmerSwitch", "capability.switchLevel", multiple: true, required: true
+  }
+}
+
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // TODO: subscribe to attributes, devices, locations, etc.
+}
+
+mappings {
+  path("/switchStatus") {
+    action: [
+      GET: "listSwitches"
+    ]
+  }
+  path("/setLevel") {
+    action: [
+      POST: "updateSwitches"
+    ]
+  }
+}
+
+def listSwitches() {
+    def resp = []
+    DimmerSwitch.each {
+      resp << [name: it.displayName, level: it.currentValue("level")]
+      
+    }
+    log.debug "level value in updateAllDimmers: $resp"
+    return resp
+}
+void updateSwitches() {
+    def level = request.JSON?.level
+    //def idData = request.JSON?.id
+    log.debug "level value in updateAllDimmers: $level"
+    //log.debug "Selected id is : $idData"
+    DimmerSwitch?.each {
+        //if(it.id == idData){
+             //log.debug "Selected Id : $it.id"
+             log.debug "Selected level : $level"
+          
+           DimmerSwitch.each{it.setLevel(level)
+           }  
+        //}
+  
+   }
+}
